7bc4521cf1a2fe39fecf3fc005b5c12635cdf7fc
ujjwal chauhan <ujjwalchauhan@ujjwals-MacBook-Air.local>
2023-07-13 15:13:48 +0530

    Intial commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose')
-const DB = 'mongodb+srv://Uj293:Abc123456@cluster0.dvn94rc.mongodb.net/Task-Manager-Api?retryWrites=true&w=majority'
-mongoose.connect(DB).then(()=>{
-    console.log('Connection is successful')
-}).catch((err)=>{
-    console.log('no connection')
-});
+const mongoose = require("mongoose");
+const DB =
+  "mongodb+srv://Uj293:Abc123456@cluster0.yy2tidg.mongodb.net/database?retryWrites=true&w=majority";
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection is successful");
+  })
+  .catch((err) => {
+    console.log("no connection");
+  });
 // mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
-
--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
-// // const bcrypt = require('bcryptjs')
+  console.log("Server is up on port " + port);
+});
+// // // const bcrypt = require('bcryptjs')
 // const jwt = require('jsonwebtoken')
-// const myFunction = async () => {
-//     const password = 'Red12345!'
-//     const hashedPassword = await bcrypt.hash(password, 8)
+// // const myFunction = async () => {
+// //     const password = 'Red12345!'
+// //     const hashedPassword = await bcrypt.hash(password, 8)
 
-//     console.log(password)
-//     console.log(hashedPassword)
+// //     console.log(password)
+// //     console.log(hashedPassword)
 
-//     const isMatch = await bcrypt.compare('red12345!', hashedPassword)
-//     console.log(isMatch)
-// }
+// //     const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+// //     console.log(isMatch)
+// // }
 // const myFunction = async ()=>{
 //      const token = jwt.sign({_id: 'Bhaba1' }, 'thisismycourse')
 //      console.log(token)
 // }
 // myFunction()
-// const Task = require('./models/task')
-// const x = async ()=>{
-//      const task = await Task.findById('645e081f81adf0ac880025c4')
-//      await task.populate('owner')
-//      console.log(task.owner)
-// }
-// x()
+// // const Task = require('./models/task')
+// // const x = async ()=>{
+// //      const task = await Task.findById('645e081f81adf0ac880025c4')
+// //      await task.populate('owner')
+// //      console.log(task.owner)
+// // }
+// // x()
